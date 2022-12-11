4d666c11cc832e63791c55beeab1337027a14b82
Parviz Karimli <devparviz@gmail.com>
2022-12-11 12:52:13 +0100

    Added JobFactory; added seeder command to create 6 jobs using JobFactory in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        \App\Models\Job::factory(6)->create();
     }
 }
