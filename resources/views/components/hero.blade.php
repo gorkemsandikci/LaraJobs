6e3adba532b5f362044890703f2b26fa38ed623f
Parviz Karimli <devparviz@gmail.com>
2022-12-15 11:25:44 +0100

    Removed "Sign Up to List a Job" link from the hero component if the user is logged in

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -14,13 +14,15 @@
         <p class="text-2xl text-gray-200 font-bold my-4">
             Find or post Laravel jobs
         </p>
-        <div>
-            <a
-                href="/register"
-                class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
-            >
-                Sign Up to List a Job
-            </a>
-        </div>
+        @guest
+            <div>
+                <a
+                    href="/register"
+                    class="inline-block border-2 border-white text-white py-2 px-4 rounded-xl uppercase mt-2 hover:text-black hover:border-black"
+                >
+                    Sign Up to List a Job
+                </a>
+            </div>
+        @endguest
     </div>
 </section>
